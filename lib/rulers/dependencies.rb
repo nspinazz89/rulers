6a2eba9b32168437f4ed9e65a033d157ab557782
nspinazz89 <nspinazz89@gmail.com>
2020-02-17 18:43:01 -0600

    fixing typo

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -5,7 +5,7 @@ class Object
     # this isn't going to cut it longterm if there is a multi-thread issue, but resolves stack too deep const_missing
     return nil if @calling_const_missing
 
-    @calling_const_missing = to_underscore
+    @calling_const_missing = true
     require Rulers.to_underscore(c.to_s)
     klass = Object.const_get(c)
     @calling_const_missing = false
