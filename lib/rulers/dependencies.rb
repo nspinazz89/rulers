d0e3f1797bd06dbd8e8d0c1a660b365469e5635f
nspinazz89 <nspinazz89@gmail.com>
2020-02-17 18:34:50 -0600

    add imperfect fix for stack too deep const missing issue

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -2,7 +2,14 @@
 
 class Object
   def self.const_missing(c)
+    # this isn't going to cut it longterm if there is a multi-thread issue, but resolves stack too deep const_missing
+    return nil if @calling_const_missing
+
+    @calling_const_missing = to_underscore
     require Rulers.to_underscore(c.to_s)
-    Object.const_get(c)
+    klass = Object.const_get(c)
+    @calling_const_missing = false
+
+    klass
   end
 end
